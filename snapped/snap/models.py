ca148d94477294244e8a6f7e98c41729914cba76
Imma7 <immamugambi@gmail.com>
2018-07-17 15:36:25 +0300

    Imported url function and apps views module

--- snapped/snap/models.py
+++ snapped/snap/models.py
@@ -1,16 +1,17 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
 class Image(models.Model):
     image = models.ImageField(upload_to = 'gallery/', blank = True)
     img_name = models.CharField(max_length = 30)
-    img_description = models.TextField(max_length=50)
+    img_description = models.TextField(max_length=50, blank=True)
     pub_date = models.DateTimeField(auto_now_add=True)
     location = models.ForeignKey('Location')
     category = models.ManyToManyField('Category')
 
     def __str__(self):
-        return self.image
+        return self.img_name
 
 class Category(models.Model):
     category_name = models.CharField(max_length = 50)
