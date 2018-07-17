d40bf916adde8a8ccfcfd1142d51206dc150dbed
Imma7 <immamugambi@gmail.com>
2018-07-17 11:43:07 +0300

    Added Bootstrap

--- snapped/snap/models.py
+++ snapped/snap/models.py
@@ -6,14 +6,20 @@ class Image(models.Model):
     img_name = models.CharField(max_length = 30)
     img_description = models.TextField(max_length=50)
     pub_date = models.DateTimeField(auto_now_add=True)
-    location = models.ForeignKey(Location)
-    category = models.ManyToManyField(category)
+    location = models.ForeignKey('Location')
+    category = models.ManyToManyField('Category')
 
     def __str__(self):
-        return self.
+        return self.image
 
 class Category(models.Model):
     category_name = models.CharField(max_length = 50)
 
+    def __str__(self):
+        return self.category_name
+
 class Location(models.Model):
     location_name = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return self.location_name
