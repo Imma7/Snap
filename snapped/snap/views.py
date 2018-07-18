26a557ab781ce95b74b569d07b36ed74b4c2dc3c
Imma7 <immamugambi@gmail.com>
2018-07-18 06:56:58 +0300

    Connected templates to the view function

--- snapped/snap/views.py
+++ snapped/snap/views.py
@@ -6,16 +6,7 @@ import datetime as dt
 def index(request):
     date = dt.date.today()
 
-    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
-    day = convert_dates(date)
-    
-    html = f'''
-        <html>
-            <body>
-                <h1 style=float:right;> Posted on {day} {date.day}-{date.month}-{date.year} </h1>
-        </html>
-            '''
-    return HttpResponse(html)
+    return render(request, 'index.html')
 
 def convert_dates(dates):
     # Function that gets the weekday number for the date.
