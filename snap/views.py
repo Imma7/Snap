265cd46bfc8e791a2db17c46ac57176dfe049f1c
Imma7 <immamugambi@gmail.com>
2018-08-08 16:37:48 +0300

    created search view funtion

--- snap/views.py
+++ snap/views.py
@@ -19,6 +19,16 @@ def get_category(request, category):
     image = Image.filter_category(category)
     return render(request, 'category.html', locals())
     
+def search_results(request):
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_image = Image.search_image(search_term)
+        message = f"{search_term}"
+        return render(request, 'search.html', {"message": message, "images":searched_image})
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html', {"message":message})
+
 
 
 def convert_dates(dates):
