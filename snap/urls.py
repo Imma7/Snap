558529eeed67878c8ef25389bc364191f2c1bbbe
Imma7 <immamugambi@gmail.com>
2018-08-08 17:57:56 +0300

    Added search url for search image

--- snap/urls.py
+++ snap/urls.py
@@ -5,10 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$', views.index, name='index'),
-    # url(r'^search/', views.search, name = 'search'),
+    url(r'^search/', views.search_results, name='search_results'),    
     url(r'^location/(?P<location>.*)',views.get_location, name = 'location'),
     url(r'^category/(?P<category>.*)', views.get_category, name = 'category'),
 ]
-
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
