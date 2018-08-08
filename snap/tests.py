13061a73d7bdf479afd1ee20af98a3e142690327
Imma7 <immamugambi@gmail.com>
2018-08-08 17:57:26 +0300

    Performed test for all the classes

--- snap/tests.py
+++ snap/tests.py
@@ -2,3 +2,44 @@ from django.test import TestCase
 from .models import Image, Location, Category
 
 # Create your tests here.
+class ImageTestClass(TestCase):
+
+    #Set up Method
+    def setUp(self):
+        self.laflare=Image(img_name = 'Path', img_description = 'Road to success')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.laflare, Image))
+
+     # Testing Save Method
+    def test_save_method(self):
+        self.laflare.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
+
+     # Testing Delete Method
+    def test_delete_method(self):
+        self.laflare.save_editor()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
+
+class LocationTestClass(TestCase):
+
+    #Set up Method
+    def setUp(self):
+        self.laflare=Location(location_name = 'Nairobi')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.laflare, Location))
+        
+class CategoryTestClass(TestCase):
+
+    #Set up Method
+    def setUp(self):
+        self.laflare=Category(category_name = 'Travel')
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.laflare, Category))
